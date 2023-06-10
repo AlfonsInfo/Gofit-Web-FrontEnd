1187f79f6ad8a24b66a0ce61426d5e2910bbef0b
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-06-10 15:26:50 +0700

    image done

--- src/data/staticData.js
+++ src/data/staticData.js
@@ -16,27 +16,27 @@ export const getImageUrl = (path) => {
 
 export const functionalKasir = [
   new functionalMenu('Olah Member',
-  'Fitur ini memungkinkan pegawai untuk menambahkan, mengubah, mengupdate, dan menghapus data member.','./assets/reset-password-0e41778b.jpg','member'),
-  new functionalMenu('Reset Password Member','Fitur ini mendukung pegawai melayani member yang ingin melakukan reset password karena lupa','../../src/assets/image/reset-password.jpg','member-reset-password'),
-  new functionalMenu('Transaksi','Fitur ini mendukung layanan transaksi seperti aktivasi membership, deposit uang, deposit kelas dan cetak struk','../../src/assets/image/transaction-gym.jpg','transaksi-member',),
-  new functionalMenu('Presensi Member','Fitur ini digunakan untuk konfirmasi kehadiran member gym dan member kelas','../../src/assets/image/presensi.jpg','presensi-member','presensi-member'),
-  new functionalMenu('Sistem','Fitur ini digunakan untuk mendukung kinerja sistem website dan apps','../../src/assets/image/presensi.jpg','presensi-member','presensi-member'),
+  'Fitur ini memungkinkan pegawai untuk menambahkan, mengubah, mengupdate, dan menghapus data member.','./assets/gym-membership-app-c9c6336e.jpeg','member'),
+  new functionalMenu('Reset Password Member','Fitur ini mendukung pegawai melayani member yang ingin melakukan reset password karena lupa','./assets/reset-password-0e41778b.jpg','member-reset-password'),
+  new functionalMenu('Transaksi','Fitur ini mendukung layanan transaksi seperti aktivasi membership, deposit uang, deposit kelas dan cetak struk','./assets/transaction-gym-10474599.jpg','transaksi-member',),
+  new functionalMenu('Presensi Member','Fitur ini digunakan untuk konfirmasi kehadiran member gym dan member kelas','./assets/image/presensi-e46a1aa1.jpg','presensi-member','presensi-member'),
+  // new functionalMenu('Sistem','Fitur ini digunakan untuk mendukung kinerja sistem website dan apps','./assets/image/presensi-e46a1aa1.jpg','presensi-member','presensi-member'),
 ];
 
 
 export const functionalAdmin = [
-  new functionalMenu('Olah Data Instruktur','Fitur Olah Data Instruktur (Menambahkan, Menampilkan, Mengubah, Menghapus) data instruktur','../src/assets/image/instruktor.jpg','instruktur')
+  new functionalMenu('Olah Data Instruktur','Fitur Olah Data Instruktur (Menambahkan, Menampilkan, Mengubah, Menghapus) data instruktur','./assets/instruktor-312beaa0.jpg','instruktur')
 ]
 export const functionalMO = [
-  new functionalMenu('Olah Jadwal Umum','Fitur pengolah data Jadwal Secara Umum, Manajer Operasional bisa menambahkankan jadwal , mengupdate jadwal, menampilkan dan menghapus jadwal','../../src/assets/image/schedule.jpg','jadwal-umum'),
-  new functionalMenu('Olah Jadwal Harian','Fitur Generate Jadwal untuk setiap 1 minggu, Fitur ini dibutuhkan untuk mendukung operasional gym setiap minggu','../../src/assets/image/daily-schedule.jpg','jadwal-harian'),
-  new functionalMenu('Perijinan Instruktur','Fitur pengolah data perijinan instruktur, MO bisa memantau perijinan instruktur, melakukan konfirmasi perijinan.','../../src/assets/image/perijinan.jpeg','ijin-instruktur','ijin-instruktur'),
-  new functionalMenu('Laporan','Fitur Laporan digunakan untuk memantau operasional Gym secara keseluruhan.','../../src/assets/image/report.jpg','laporan')
+  new functionalMenu('Olah Jadwal Umum','Fitur pengolah data Jadwal Secara Umum, Manajer Operasional bisa menambahkankan jadwal , mengupdate jadwal, menampilkan dan menghapus jadwal','./assets/schedule-130e01a6.jpg','jadwal-umum'),
+  new functionalMenu('Olah Jadwal Harian','Fitur Generate Jadwal untuk setiap 1 minggu, Fitur ini dibutuhkan untuk mendukung operasional gym setiap minggu','./assets/daily-schedule-05721eb6.jpg','jadwal-harian'),
+  new functionalMenu('Perijinan Instruktur','Fitur pengolah data perijinan instruktur, MO bisa memantau perijinan instruktur, melakukan konfirmasi perijinan.','./assets/perijinan-3d8e413d.jpeg','ijin-instruktur','ijin-instruktur'),
+  new functionalMenu('Laporan','Fitur Laporan digunakan untuk memantau operasional Gym secara keseluruhan.','./assets/report-b3e8deec.jpg','laporan')
 ]
 
 export const menuPresensi = [
-  new functionalMenu('Presensi Member Gym', 'Fitur ini digunakan untuk mengecek dan mengkonfirmasi kehadiran peserta gym' , '../../src/assets/image/presensi.jpg','presensi-member-gym'),
-  new functionalMenu('Presensi Peserta Kelas', 'Fitur ini digunakan untuk mengecek kehadiran member disetiap kelas yang diikutinnya' , '../../src/assets/image/presensi.jpg','presensi-member-kelas')
+  new functionalMenu('Presensi Member Gym', 'Fitur ini digunakan untuk mengecek dan mengkonfirmasi kehadiran peserta gym' , './assets/presensi-e46a1aa1.jpg','presensi-member-gym'),
+  new functionalMenu('Presensi Peserta Kelas', 'Fitur ini digunakan untuk mengecek kehadiran member disetiap kelas yang diikutinnya' , './assets/presensi-e46a1aa1.jpg','presensi-member-kelas')
 ]
 export const menuTransaksi = [
   new functionalMenu('Transaksi Aktivasi', 'Fitur ini digunakan untuk melakukan aktivasi akun member' , '../../src/assets/image/activate.png','transaksi-aktivasi'),
@@ -46,9 +46,9 @@ export const menuTransaksi = [
 
 
 export const menuLaporan = [
-  new functionalMenu('Pendapatan Bulanan Per Tahun', 'Fitur ini digunakan untuk menghasilkan laporan pendapatan bulanan dalam suatu tahun' , '../../src/assets/image/report.jpg','laporan-tahunan'),
-  new functionalMenu('Laporan Aktivitas Kelas Bulanan', 'Fitur ini digunakan untuk menghasilkan aktivitas kelas dalam satu bulan' , '../../src/assets/image/classgym.jpeg','laporan-kelas-bulanan'),
-  new functionalMenu('Laporan Aktivitas Gym Bulanan', 'Fitur ini digunakan untuk menghasilkan aktivitas gym dalam satu bulan' , '../../src/assets/image/background-1.jpg','laporan-gym-bulanan'),
-  new functionalMenu('Kinerja Instruktur Bulanan', 'Fitur ini digunakan untuk menampilkan kinerja instruktur' , '../../src/assets/image/instruktor.jpg','laporan-instruktur-bulanan')
+  new functionalMenu('Pendapatan Bulanan Per Tahun', 'Fitur ini digunakan untuk menghasilkan laporan pendapatan bulanan dalam suatu tahun' , './assets/report-b3e8deec.jpg','laporan-tahunan'),
+  new functionalMenu('Laporan Aktivitas Kelas Bulanan', 'Fitur ini digunakan untuk menghasilkan aktivitas kelas dalam satu bulan' , './assets/background-1-a080f3fa.jpg','laporan-kelas-bulanan'),
+  new functionalMenu('Laporan Aktivitas Gym Bulanan', 'Fitur ini digunakan untuk menghasilkan aktivitas gym dalam satu bulan' , './assets/background-1-a080f3fa.jpg','laporan-gym-bulanan'),
+  new functionalMenu('Kinerja Instruktur Bulanan', 'Fitur ini digunakan untuk menampilkan kinerja instruktur' , './assets/instruktor-312beaa0.jpg','laporan-instruktur-bulanan')
   // new functionalMenu('Laporan Aktivitas Kelas Bulanan', 'Fitur ini digunakan untuk menghasilkan aktivitas kelas dalam satu bulan' , '../../src/assets/image/classgym.jpeg','transaksi-deposit-kelas'),
 ]
